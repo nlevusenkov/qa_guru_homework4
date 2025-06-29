5fa40555975a97cc53daab26ef998f62a5943826
Nikita <rock25102@gmail.com>
2025-06-29 21:52:47 +0400

    улучшение кода

--- qa_guru_homework4_test/registration_page.py
+++ qa_guru_homework4_test/registration_page.py
@@ -11,19 +11,23 @@ class RegistrationForm:
         browser.element('#userEmail').type(email)
     def fill_phone_number(self, phone):
         browser.element('#userNumber').type(phone)
+    @property
     def fill_DateBithday(self):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type('May').click()
         browser.element('.react-datepicker__year-select').type('2000').click()
         browser.element('.react-datepicker__day--010').click()
+    @property
     def fill_gender(self):
         browser.element('[for="gender-radio-1"]').click()
     def fill_subjectsInput(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
+    @property
     def fill_Hobbies(self):
         time.sleep(0.5)
         browser.element('[for="hobbies-checkbox-2"]').click()
-    def fill_image(self):
+    @property
+    def upload_image(self):
         browser.element('#uploadPicture').send_keys(os.path.abspath('../image/fabula-ai.png'))
     def fill_CurrentAddress(self, Address):
         browser.element('[placeholder="Current Address"').type(Address)
@@ -33,6 +37,8 @@ class RegistrationForm:
     def fill_city(self, city):
         browser.element('#city').click()
         browser.all('.css-26l3qy-menu div').element_by(have.exact_text(city)).click()
+
+    @property
     def form_sibmit(self):
         browser.element('[type=submit]').click()
 
--- tests/test_form_selene.py
+++ tests/test_form_selene.py
@@ -10,16 +10,16 @@ def test_form_selene_1(setting_browser):
     form_registration.fill_firstname('John')
     form_registration.fill_lastname('Doe')
     form_registration.fill_email_address('test@test.ru')
-    form_registration.fill_gender()
+    form_registration.fill_gender
     form_registration.fill_phone_number('7999999999')
-    form_registration.fill_DateBithday()
+    form_registration.fill_DateBithday
     form_registration.fill_subjectsInput('English')
-    form_registration.fill_Hobbies()
-    form_registration.fill_image()
+    form_registration.fill_Hobbies
+    form_registration.upload_image
     form_registration.fill_CurrentAddress('test')
     form_registration.fill_state("Uttar Pradesh")
     form_registration.fill_city('Merrut')
-    form_registration.form_sibmit()
+    form_registration.form_sibmit
     form_registration.should_registreded_user_with(full_name='John Doe',
                                                    email='test@test.ru',
                                                    gender='Male',
