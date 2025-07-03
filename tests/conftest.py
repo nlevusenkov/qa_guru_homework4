0a442b9ab44087767ae520538737a036155fbe1a
Nikita <rock25102@gmail.com>
2025-07-03 21:45:00 +0400

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope='function')
 def setting_browser():
     driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.window_width = 1920
