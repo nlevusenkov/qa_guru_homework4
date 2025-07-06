e53ec7e44fa8de3b55d1c378f5f774a9ebb83c98
Nikita <rock25102@gmail.com>
2025-07-06 20:26:16 +0400

    attach add

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,5 +27,7 @@ def setting_browser(request):
 
     yield browser
     attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_screenshot(browser)
     attach.add_video(browser)
     browser.quit()
