802b18bf226321d472dbadda74c8aeaf3f145698
Nikita <rock25102@gmail.com>
2025-07-06 20:33:09 +0400

    update attach.py

--- utils/attach.py
+++ utils/attach.py
@@ -2,23 +2,28 @@ import allure
 from allure_commons.types import AttachmentType
 
 
+# Скриншоты
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
-
+# логи
 def add_logs(browser):
-    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
-    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+    try:
+       logs = browser.execute("getlog", {"type": 'browser'})["value"]
+       log_text = "\n".join(str(log) for log in logs)
+    except Exception as e:
+       log_text = f"Логи недоступны: {e}"
+    allure.attach(log_text, 'browser_logs', AttachmentType.TEXT)
 
-
+# html-код страницы
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
 def add_video(browser):
-    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    video_url = f"https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
