6d2eb49d4fd769657a7717f8d187ac1410444f05
Nikita <rock25102@gmail.com>
2025-07-06 20:34:53 +0400

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,6 @@ def setting_browser(request):
     yield browser
     attach.add_screenshot(browser)
     attach.add_logs(browser)
-    attach.add_screenshot(browser)
+    attach.add_html(browser)
     attach.add_video(browser)
     browser.quit()
